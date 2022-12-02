5373232d76c9f3513e7df93d31b51f3e0135f318
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2022-12-01 20:34:31 -0500

    something wrong with routes!

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 //asynchronous processes
 const readFileAsync = util.promisify(fs.readFile);
 
-const Port = process.env.Port || 9001;
+const Port = process.env.PORT || 9001;
 const app = express();
 
 //f(x) to create a new note for client
@@ -25,14 +25,21 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //ROUTE to/for HOMEPAGE move to routes js
+app.get("/notes",  (req, res) =>
+res.sendFile(path.join(_dirname,  './public/notes.html'))
+);
 // app.get('/', (req, res)  =>
 // res.sendFile(path.join(_dirname, './public/index.html'))
 // );
+app.get("/",  (req, res)  {
+    res.sendFile(path.join(_dirname, "./public/index.html"))
+});
+ 
+app.get("*",  (req, res)  {
+    res.sendFile(path.join(_dirname, "./public/index.html"))
+});
 
-// app.get('/notes',  (req, res) =>
-// res.sendFile(path.join(_dirname,  './public/notes.html'))
-// );
-
+//does this have to go first/on top?
 app.get("/api/notes", (req, res) =>{
 readFileAsync('./db/db.json', "utf8").then(function(data){
     notes = [].concat(JSON.parse(data))
@@ -51,7 +58,12 @@ app.post("/api/notes", (req, res) {
     });
 })
 
-//keep here for order?
+
+
+/// DELETE  NOTES and output new array of ones you dont delete
+
+
+//keep here for order? LISTENING
 app.listen(PORT, () => {
 console.log (`App listening on http://localhost:${PORT}`);
 });
